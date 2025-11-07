d21b5c26b7f4d9757ea55f894fc6967b0b8a352f
aregmk6 <areg631@gmail.com>
2025-11-07 16:02:53 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,59 @@
+print("sourced init.lua, advent of code - tutorial")
+
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- source lines in buffer
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- travers QuickFix manu
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
+-- open parent directory with oil
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<M-->", function()
+  vim.cmd("new")
+  vim.cmd("Oil")
+end, { desc = "Open parent directory" })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- when the terminal opens it removes the line numbers
+vim.api.nvim_create_autocmd('TermOpen', {
+  desc = 'Terminal Opening Config',
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+-- open terminal
+local job_id = 0
+vim.keymap.set("n", "<space>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
+  job_id = vim.bo.channel
+end)
+
+-- open terminal and run 'make' command
+vim.keymap.set("n", "<space>tm", function()
+  vim.fn.chansend(job_id, { "make\r" })
+end)
